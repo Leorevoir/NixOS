a322226e6f5495155014524de93b0b6da8ae7c53
= <=>
2025-01-07 20:29:39 +0100

    fix: typo issue for eza & bat

--- modules/programs/cli/bat/default.nix
+++ modules/programs/cli/bat/default.nix
@@ -1,23 +1,48 @@
+# { pkgs, ... }:
+# {
+#   home-manager.sharedModules = [
+#     (_: {
+#       programs.bat = {
+#         enable = true;
+#       };
+#       home = {
+#         file = {
+#           ".config/bat/theme/Catppuccin Mocha.tmTheme".text = builtins.readFile (pkgs.fetchurl {
+#             url = "https://raw.githubusercontent.com/catppuccin/bat/refs/heads/main/themes/Catppuccin%20Macchiato.tmTheme";
+#             sha256 = "zL18U4AXMO8+gBH3T/HDl8e7OYjIRqUdeeb0i4V7kVI=";
+#           });
+#           ".config/bat/config".text = ''
+#             --theme="Catppuccin Mocha"
+#           '';
+#         };
+#         shellAliases = {
+#           cat = "bat";
+#         };
+#       };
+#     })
+#   ];
+# }
 { pkgs, ... }:
 {
   home-manager.sharedModules = [
     (_: {
       programs.bat = {
         enable = true;
+        config = {
+          theme = "Catppuccin Mocha";
+        };
+        themes = {
+          "Catppuccin Mocha" = {
+            src = pkgs.fetchurl {
+              url = "https://raw.githubusercontent.com/catppuccin/bat/refs/heads/main/themes/Catppuccin%20Macchiato.tmTheme";
+              sha256 = "zL18U4AXMO8+gBH3T/HDl8e7OYjIRqUdeeb0i4V7kVI=";
+            };
+          };
+        };
       };
-      home = {
-        file = {
-          ".config/bat/theme/Catppuccin Mocha.tmTheme".text = builtins.readFile (pkgs.fetchurl {
-            url = "https://raw.githubusercontent.com/catppuccin/bat/refs/heads/main/themes/Catppuccin%20Macchiato.tmTheme";
-            sha256 = "zL18U4AXMO8+gBH3T/HDl8e7OYjIRqUdeeb0i4V7kVI=";
-          });
-          ".config/bat/config".text = ''
-            --theme="Catppuccin Mocha"
-          '';
-        };
-        shellAliases = {
-          cat = "bat";
-        };
+
+      home.shellAliases = {
+        cat = "bat";
       };
     })
   ];
--- modules/programs/cli/eza/default.nix
+++ modules/programs/cli/eza/default.nix
@@ -4,7 +4,7 @@
 {
   home-manager.sharedModules = [
     (_: {
-      programs.bat = {
+      programs.eza = {
         enable = true;
       };
       home = {
@@ -12,11 +12,11 @@
           ".config/eza/theme.yml".source = ./mocha.yml;
         };
         shellAliases = with pkgs; {
-          l = "${eza}/bin/eza -lh  --icons=auto";
-          ls = "${eza}/bin/eza   --icons=auto";
-          ll = "${eza}/bin/eza -lha --icons=auto --sort=name --group-directories-first";
-          ld = "${eza}/bin/eza -lhD --icons=auto";
-          tree = "${eza}/bin/eza --icons=auto --tree";
+          l = "${eza}/bin/eza -lh  --icons=always";
+          ls = "${eza}/bin/eza   --icons=always";
+          ll = "${eza}/bin/eza -lha --icons=always --sort=name --group-directories-first";
+          ld = "${eza}/bin/eza -lhD --icons=always";
+          tree = "${eza}/bin/eza --icons=always --tree";
         };
       };
     })
